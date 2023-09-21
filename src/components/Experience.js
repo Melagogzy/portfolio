db6899e975d105a23abdd82ff0e0113169bdd9d8
Melagogzy <ogbonnamichael21@gmail.com>
2023-09-21 04:46:04 +0100

    query for mobile and tablet responsiveness

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,7 +6,7 @@ const Details = ({position, company, companyLink, time, address, work }) => {
        const ref = useRef(null);
      return(
         <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] 
-        mx-auto flex flex-col  justify-between'>
+        mx-auto flex flex-col  justify-between md:w-[80%]'>
             <LiIcons reference={ref}/>
             <motion.div
             initial={{y:50}}
@@ -16,15 +16,15 @@ const Details = ({position, company, companyLink, time, address, work }) => {
             
             
             >
-                <h3 className='capitalize font-bold text-2xl'>{position}&nbsp; <a href={companyLink}
+                <h3 className='capitalize font-bold text-2xl sm:text-xl xs:text-lg'>{position}&nbsp; <a href={companyLink}
                 target='_blank'
-                className='text-primary capitalize'
+                className='text-primary dark: text-primaryDark capitalize'
                 >@{company}</a></h3>
             </motion.div>
-            <span className='capitalize font-medium text-dark/75'>
+            <span className='capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm'>
                 {time} | {address}
              </span>
-             <p className='font-medium w-full'>
+             <p className='font-medium w-full md:text-sm'>
                {work} 
              </p>
             
@@ -46,19 +46,21 @@ const Experience = () => {
     )
   return (
     <div className='my-64'>
-      <h2 className='font-bold text-8xl mb-32 w-full text-center'>
+      <h2 className='font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16'>
           Experience
       </h2>
       
-      <div ref={ref} className='w-[75%] mx-auto relative '>
+      <div ref={ref} className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
 
          < motion.div 
            style={{scaleY: scrollYProgress}}
-         className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top'/>
+         className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light
+         md:w-[2px] md:left-[30px] xs:left-[20px]
+         '/>
 
 
 
-           <ul className='w-full flex flex-col items-start justify-between ml-4'>
+           <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2'>
             <Details 
             position="Software Engineer" company= "Google"
             companyLink="www.formidabletech.com.ng"
